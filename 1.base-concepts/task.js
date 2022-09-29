681487c0051789759e55a4910e17d6b8f26ee25b
molottva <molottva@list.ru>
2022-09-29 23:31:43 +0300

    Первая задача

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,30 @@
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let arr = [];
+  let disc = b * b - 4 * a * c;
+  if (disc > 0) {
+    arr.push((-b + Math.sqrt(disc)) / 2 / a);
+    arr.push((-b - Math.sqrt(disc)) / 2 / a);
+  } else if (disc == 0) {
+    arr.push(-b / 2 / a);
+  }
+  return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
+  percent = parseFloat(percent);
+  contribution = parseInt(contribution);
+  amount = parseInt(amount);
+  //todo added validate
 
-  // код для задачи №2 писать здесь
+  let endDate = new Date(date);
+  let nowDate = new Date(Date.now);
+  let totalMonth = (endDate.getFullYear - nowDate.getFullYear) * 12 + endDate.getMonth - nowDate.getMonth;
+  let percentPerMonth = percent / 12;
 
+  amountPerMonth = amount * (percentPerMonth + (percentPerMonth / (Math.pow(1 + percentPerMonth, totalMonth))));
+  totalAmount = (amountPerMonth * totalAmount).toFixed(2);
   return totalAmount;
 }
